3f8d7795c2000bc8f48f833cb21affedf30baecf
Leyundai <1406221kremer@zuyd.nl>
2016-11-23 12:13:26 +0100

    Voorbeeld toegevoegd , initial release

---
+++ VoorbeeldEF/VoorbeeldEF/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace VoorbeeldEF.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ VoorbeeldEF/VoorbeeldEF/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VoorbeeldEF.Global" Language="C#" %>
